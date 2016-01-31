4e380d9ad9041e343ac856863a2be72e72dbb73d
Simon Swain <simon@simonswain.com>
2016-01-31 07:11:45 +0000

    Add system caps and read_cpu_temp hook

---
+++ caps/system.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "cloud": {
+    "rest": "https://device.zentri.com",
+    "ws": "wss://device.zentri.com"
+  },
+  "functions": [
+    {
+      "slug": "cpu_temp",
+      "title": "CPU Temperature",
+      "schema": "float",
+      "panel": "chart",
+      "hook": "read_cpu_temp",
+      "interval": 15000
+    }
+  ]
+}
---
+++ hooks/read_cpu_temp.js
@@ -0,0 +1,32 @@
+var exec = require('child_process').exec;
+var fs = require('fs');
+var moment = require('moment');
+
+var get = function(opts, done){
+
+  var cmd;
+  cmd = 'cat /sys/class/thermal/thermal_zone0/temp';
+  exec(cmd, function(error, stdout, stderr){
+
+    console.log('read cpu temp webcam', temp);
+
+    var temp = Number(stdout);
+    if(isNaN(temp)){
+      return done();
+    }
+    temp = Number((temp / 1000).toFixed(2));
+    
+    var message  = {
+      at: new Date().getTime(),
+      value: temp
+    };
+
+    done(null, message);
+
+  });
+
+};
+
+module.exports = {
+  get: get
+};
