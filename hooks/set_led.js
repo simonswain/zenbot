ac30e8dbc3ece2e661a52135d3f0d391e1a0dd49
Simon Swain <simon@simonswain.com>
2016-02-12 21:21:18 +1100

    Updated

--- hooks/set_led.js
+++ hooks/set_led.js
@@ -1,7 +1,6 @@
 var exec = require('child_process').exec;
 
 var put = function(opts, message, done){
-  console.log(message);
   var cmd;
   cmd = '/usr/bin/python /home/pi/led_status.py ' + message.value;
   exec(cmd, function(error, stdout, stderr){
