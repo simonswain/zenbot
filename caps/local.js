a3915ed6e5de180f38c3913f64e075d4f7b1cfbc
Simon Swain <simon@simonswain.com>
2016-02-19 17:12:44 +1100

    Additional streams

--- caps/local.js
+++ caps/local.js
@@ -8,16 +8,33 @@ module.exports = {
     "title": "Status LED",
     "schema": "boolean",
     "panel": "toggle",
-    "hook": "set_value"
-  },
-  {
+    "hook": "set_value",
+    "norepeat": true
+  }, {
+    "stream": "random_value",
+    "title": "Random Value",
+    "schema": "float",
+    "panel": "chart",
+    "hook": "random_value",
+    "norepeat": true,
+    "interval": 5000
+  }, {
     "stream": "key_press",
     "title": "Key Press",
     "schema": "boolean",
     "panel": "toggle",
     "hook": "key_toggle",
+    "norepeat": true,
     "opts": {
       "key": ' '
     }
+  }, {
+    "stream": "cpu_temp",
+    "title": "CPU Temperature",
+    "schema": "float",
+    "panel": "chart",
+    "hook": "read_cpu_temp",
+    "interval": 15000,
+    "norepeat": true
   }]
 };
